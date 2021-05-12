f3eb5f92def185e3c0db32f0e8c5ea1b6bee40d5
EC2 Default User <ec2-user@ip-172-31-3-23.us-west-2.compute.internal>
2021-05-12 03:42:09 +0000

    Gatby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Meipaly Gatsby Js Template`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Podpad Template`,
+    description: `Podpad`,
     author: `@gatsbyjs`,
   },
   plugins: [
